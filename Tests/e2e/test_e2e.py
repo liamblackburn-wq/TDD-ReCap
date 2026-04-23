f2263063d3771ae7694e50db085032342628ebdf
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-04-23 15:34:07 +0100

    refactor folder structure

--- Tests/e2e/test_e2e.py
+++ Tests/e2e/test_e2e.py
@@ -1,23 +1,23 @@
 from playwright.sync_api import Page, expect
-from app import app as flask_app
+from app import app as my_app
 import pytest
 
-test_app = flask_app.test_client()
 
-test_app.get('/')
 
 @pytest.fixture(scope="session")
 def app():
-    return flask_app
+    return my_app
 
-def test_header_exists(page: Page, live_server):
+
+def test_heading_text_exists(page: Page, live_server):
     page.goto(live_server.url())
     expect(page.get_by_role("heading", name="Apprentice Duties")).to_contain_text('Apprentice Duties')
 
 def test_home_page_is_reachable():
-    response = test_app.get('/')
-    assert response.status_code is 200
+    with my_app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
 
 # def test_response_returns_ok(page: Page):
-#     response = page.request.get("http://127.0.0.1:5000")
+#     response = test_app.get("http://127.0.0.1:5000")
 #     expect(response).to_be_ok()
--- db.py
+++ db.py
@@ -1,2 +1,14 @@
+class Duties:
+    def __init__(self, name, description):
+        self.name = name
+        self._description = description
+
+    def equals(self, duty2):
+        return self.name == duty2.name
+
+    def description(self):
+        return self._description
+
+
 def call_database():
     pass
