98afa74705690ddbf60b4bb2b0a4d0979ef1c1f8
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-04-23 16:34:58 +0100

    add failing e2e test

--- Tests/e2e/test_e2e.py
+++ Tests/e2e/test_e2e.py
@@ -9,15 +9,15 @@ def app():
     return my_app
 
 
-def test_heading_text_exists(page: Page, live_server):
+def xtest_heading_text_exists(page: Page, live_server):
     page.goto(live_server.url())
     expect(page.get_by_role("heading", name="Apprentice Duties")).to_contain_text('Apprentice Duties')
 
-def test_home_page_is_reachable():
+def xtest_home_page_is_reachable():
     with my_app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
 
-# def test_response_returns_ok(page: Page):
-#     response = test_app.get("http://127.0.0.1:5000")
-#     expect(response).to_be_ok()
+def test_add_new_duties_button_is_visible(page: Page, live_server):
+    page.goto(live_server.url())
+    expect(page.get_by_role("button", name="Add New Duty")).to_be_visible()
