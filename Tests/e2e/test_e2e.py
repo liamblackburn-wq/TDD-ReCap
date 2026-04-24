e886cbec20ae80880dcc56bc05b9d61f149e95f2
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-04-24 10:24:24 +0100

    refactor using pytest fixtures

--- Tests/e2e/test_e2e.py
+++ Tests/e2e/test_e2e.py
@@ -8,35 +8,36 @@ import pytest
 def app():
     return my_app
 
-
-def xtest_heading_text_exists(page: Page, live_server):
+@pytest.fixture
+def homepage(page: Page, live_server):
     page.goto(live_server.url())
-    expect(page.get_by_role("heading", name="Apprentice Duties")).to_contain_text('Apprentice Duties')
+    return page
+
+@pytest.fixture
+def revealed_form(homepage: Page):
+    homepage.get_by_role("button", name="Add Duties").click()
+    return homepage
 
 def xtest_home_page_is_reachable():
     with my_app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
 
-def xtest_add_duties_button_is_visible_and_clickable(page: Page, live_server):
-    page.goto(live_server.url())
+def test_heading_text_exists(homepage: Page):
+    expect(homepage.get_by_role("heading", name="Apprentice Duties")).to_contain_text('Apprentice Duties')
 
-    add_duties_button = page.get_by_role("button", name="Add Duties")
+def test_add_duties_button_is_visible_and_clickable(homepage: Page):
+
+    add_duties_button = homepage.get_by_role("button", name="Add Duties")
     expect(add_duties_button).to_be_visible()
     add_duties_button.click()
-    expect(page.get_by_role("form", name="Add Duty Form" )).to_be_visible()
+    expect(homepage.get_by_role("form", name="Add Duty Form" )).to_be_visible()
 
-def xtest_correct_amount_of_duty_checkboxes(page: Page, live_server):
-    page.goto(live_server.url())
-    add_duties_button = page.get_by_role("button", name="Add Duties")
-    add_duties_button.click()
-    checkboxes = page.get_by_role("checkbox")
+def test_correct_amount_of_duty_checkboxes(revealed_form: Page):
+    checkboxes = revealed_form.get_by_role("checkbox")
     expect(checkboxes).to_have_count(13)
 
-def test_individual_duty_exists_and_checked(page: Page, live_server):
-    page.goto(live_server.url())
-    add_duties_button = page.get_by_role("button", name="Add Duties")
-    add_duties_button.click()
-    duty_5_check = page.get_by_label("Duty 5")
+def test_individual_duty_exists_and_checked(revealed_form: Page):
+    duty_5_check = revealed_form.get_by_label("Duty 5")
     duty_5_check.check()
     expect(duty_5_check).to_be_checked()
