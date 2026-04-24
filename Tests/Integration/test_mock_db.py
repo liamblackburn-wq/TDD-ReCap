e425720a5a76176df8ad43657f4816143e553be8
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-04-24 15:12:20 +0100

    changed course of testing following call with lead engineer. Now trying sqlite3 with unittest mock

--- Tests/Integration/test_mock_db.py
+++ Tests/Integration/test_mock_db.py
@@ -1,2 +1,29 @@
-from db import Duties
+from db import DatabaseService
+import pytest
+import sqlite3
+from unittest.mock import Mock
+
+def mock_sql_connection(rows_to_return):
+    con = Mock()
+    cursor = Mock()
+    result_set = Mock()
+    result_set.fetchall.return_value = rows_to_return
+
+    cursor.execute.return_value = result_set
+    con.cursor.return_value = cursor
+
+@pytest.mark.parametrize("duty_id_list, expected_result", (
+        ([1, 2, 3], ["Duty 1", "Duty 2", "Duty 3"]),
+        ([1, 3], ["Duty 1", "Duty 3"])
+))
+def xtest_get_duty_descriptions_returns_descriptions(duty_id_list, expected_result):
+    # rows_to_return =
+    # iterate over duty_id list and result, zip together.
+    # python zip function maybe?
+
+    db = DatabaseService(mock_sql_connection(rows_to_return))
+
+    actual_result = db.get_duty_descriptions(duty_id_list)
+
+    assert actual_result == expected_result
 
--- db.py
+++ db.py
@@ -1,14 +1,3 @@
-class Duties:
-    def __init__(self, name, description):
-        self.name = name
-        self._description = description
-
-    def equals(self, duty2):
-        return self.name == duty2.name
-
-    def description(self):
-        return self._description
-
-
-def call_database():
-    pass
+class DatabaseService:
+    def get_duty_descriptions(self, duty_ids):
+        return ["Duty 1", "Duty 2", "Duty 3"]
