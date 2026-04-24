56e7c6677b9cc3d0f454d4f28a81e68a643d15b1
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-04-24 11:26:01 +0100

    failing test for form submit button

--- Tests/e2e/test_e2e.py
+++ Tests/e2e/test_e2e.py
@@ -23,21 +23,32 @@ def xtest_home_page_is_reachable():
         response = client.get('/')
         assert response.status_code == 200
 
-def test_heading_text_exists(homepage: Page):
+def xtest_heading_text_exists(homepage: Page):
     expect(homepage.get_by_role("heading", name="Apprentice Duties")).to_contain_text('Apprentice Duties')
 
-def test_add_duties_button_is_visible_and_clickable(homepage: Page):
-
+def xtest_add_duties_button_is_visible_and_reveals_form_when_clicked(homepage: Page):
     add_duties_button = homepage.get_by_role("button", name="Add Duties")
     expect(add_duties_button).to_be_visible()
     add_duties_button.click()
     expect(homepage.get_by_role("form", name="Add Duty Form" )).to_be_visible()
 
-def test_correct_amount_of_duty_checkboxes(revealed_form: Page):
+def xtest_correct_amount_of_duty_checkboxes(revealed_form: Page):
     checkboxes = revealed_form.get_by_role("checkbox")
     expect(checkboxes).to_have_count(13)
 
-def test_individual_duty_exists_and_checked(revealed_form: Page):
-    duty_5_check = revealed_form.get_by_label("Duty 5")
-    duty_5_check.check()
-    expect(duty_5_check).to_be_checked()
+def xtest_individual_duty_exists_and_checked(revealed_form: Page):
+    duty_5 = revealed_form.get_by_label("Duty 5")
+    duty_5.check()
+    expect(duty_5).to_be_checked()
+
+def test_submit_button(revealed_form: Page):
+    automate_duties_list = revealed_form.get_by_role("list")
+    submit_button = revealed_form.get_by_role("button", name="Submit")
+    duty_5 = revealed_form.get_by_label("Duty 5")
+
+    duty_5.check()
+    submit_button.click()
+
+    expect(submit_button).to_be_visible()
+    expect(automate_duties_list).to_be_visible()
+    expect(automate_duties_list).to_contain_text("Duty 5")
