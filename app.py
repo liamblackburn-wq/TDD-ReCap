678530a35e64c2f200c175ac4f3af7963d40efcf
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-04-24 11:57:57 +0100

    prep flask to receive and pass back added duties

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     duty_list = [f"Duty {i}" for i in range(1, 14)]
-    return render_template('index.html', duties=duty_list)
+    added_duties = []
+
+    if request.method == "POST":
+        added_duties = request.form.getlist("duties")
+
+
+    return render_template('index.html', duties=duty_list, added_duties=added_duties)
