e824c1cdac57112710b8795cba611aa0cb9a7f1a
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-04-24 15:29:52 +0100

    refactor code. refactor tests. failing state. refactor class required

--- Tests/Integration/test_mock_db.py
+++ Tests/Integration/test_mock_db.py
@@ -12,16 +12,17 @@ def mock_sql_connection(rows_to_return):
     cursor.execute.return_value = result_set
     con.cursor.return_value = cursor
 
+    return con
+
 @pytest.mark.parametrize("duty_id_list, expected_result", (
         ([1, 2, 3], ["Duty 1", "Duty 2", "Duty 3"]),
         ([1, 3], ["Duty 1", "Duty 3"])
 ))
-def xtest_get_duty_descriptions_returns_descriptions(duty_id_list, expected_result):
-    # rows_to_return =
-    # iterate over duty_id list and result, zip together.
-    # python zip function maybe?
+def test_get_duty_descriptions_returns_descriptions(duty_id_list, expected_result):
+    rows_to_return = [(description,) for description in expected_result]
+    mock_con = mock_sql_connection(rows_to_return)
 
-    db = DatabaseService(mock_sql_connection(rows_to_return))
+    db = DatabaseService(mock_con)
 
     actual_result = db.get_duty_descriptions(duty_id_list)
 
