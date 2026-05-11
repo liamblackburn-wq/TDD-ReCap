5d28a7e66b1cacabf6cd83ef4c3b89ab595e70a8
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-05-11 15:36:23 +0100

    display duties on frontend

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from db import DatabaseService
 
 app = Flask(__name__)
 
+connection = sqlite3.connect('duties.db', check_same_thread=False)
+db_service = DatabaseService(connection)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -11,6 +13,9 @@ def home():
     added_duties = []
 
     if request.method == "POST":
-        added_duties = request.form.getlist("duties")
+        selected_duties = request.form.getlist("duties")
+        selected_ids = [int(name.split(" ")[1]) for name in selected_duties]
+
+        added_duties = db_service.get_duty_descriptions(selected_ids)
 
     return render_template('index.html', duties=duty_list, added_duties=added_duties)
