1299024cc4976fe842d4abea5b6e6911a023f688
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-06-12 18:43:57 +0100

    test duty description

---
+++ Tests/Unit/test_duty.py
@@ -0,0 +1,9 @@
+from duties import Duty
+
+def test_duty_name():
+    test_duty = Duty("Duty 1")
+    assert test_duty.name ==  "Duty 1"
+
+def test_duty_description():
+    test_duty = Duty("Duty 1", "Test Description")
+    test_duty.description = "Test Description"
