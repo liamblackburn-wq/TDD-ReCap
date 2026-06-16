513a380f7f98696e7947fd12005052d297c3e6ec
Liam Blackburn <liam.blackburn2@justice.gov.uk>
2026-06-16 08:57:01 +0100

    tests for coins base table and coins duties junction table

---
+++ Tests/Integration/test_coins_duties_model.py
@@ -0,0 +1,25 @@
+import uuid
+import pytest
+from src.models import CoinsDutiesJunction
+
+
+def test_associate_duty_to_coin(test_coin, test_duty, client):
+    payload = {
+        "coin_id": str(test_coin.id),
+        "duty_id": str(test_duty.id)
+    }
+
+    response = client.post('/coin-duties', json=payload)
+    assert response.status_code == 201
+
+    associated_ids = CoinsDutiesJunction.select().where(
+        CoinsDutiesJunction.coin == test_coin.id,
+        CoinsDutiesJunction.duty == test_duty.id
+    ).exists()
+
+    assert associated_ids == True
+
+    CoinsDutiesJunction.delete().where(
+        CoinsDutiesJunction.coin == test_coin.id,
+        CoinsDutiesJunction.duty == test_duty.id
+    ).execute()
---
+++ Tests/Integration/test_coins_model.py
@@ -0,0 +1,46 @@
+import uuid
+from src.models import Coin
+
+
+def test_get_coins_endpoint(client, test_coin):
+    # ACT: Hit the Flask endpoint. Hit it HARD
+    response = client.get('/coins')
+    assert response.status_code == 200
+
+    # ASSERT: Gimme data, verify it matches
+    data = response.get_json()
+    print(data)
+    returned_coin = next(item for item in data if item['id'] == str(test_coin.id))
+
+    assert returned_coin['id'] == str(test_coin.id)
+    assert returned_coin['name'] == 'COIN_TEST'
+
+
+def test_add_coin(client):
+    payload = {
+        'id': str(uuid.uuid4()),
+        'name': 'POST_COIN_TEST'
+    }
+
+    response = client.post('/coins', json=payload)
+    assert response.status_code == 201
+
+    data = response.get_json()
+    print(data)
+    assert data['name'] == 'POST_COIN_TEST'
+    assert data['id'] == str(payload['id'])
+
+    Coin.delete().where(Coin.id == payload['id']).execute()
+
+
+def test_db_can_not_have_duplicate_coin_names(client, test_coin):
+    # first coin created in pytest test_coin fixture
+    payload = {
+        'id': str(uuid.uuid4()),
+        'name': 'COIN_TEST'
+    }
+
+    response = client.post('/coins', json=payload)
+    data = response.get_json()
+    assert response.status_code == 409
+    assert data['error'] == 'Coin already exists'
