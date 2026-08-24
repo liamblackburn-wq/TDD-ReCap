24d99de402de211543fbc112e8ec291cd6eebbdc
Liam <liam.blackburn@madetech.com>
2026-08-24 08:51:49 +0100

    fix deployed version, does not effect local flask app

--- app.py
+++ app.py
@@ -79,6 +79,12 @@ def request_logger(response):
 
     return response
 
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 
 @app.teardown_request
 def _db_close(exc):
